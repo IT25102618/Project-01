d9ac8e7f951a5fb5f4433d6b470e27eae4137f9e
Wanshika Samaranayake <it25101630@my.sliit.lk>
2026-05-18 01:26:48 +0530

    Update FileHandler.java

--- src/main/java/com/demo/parkingmanagementsystem/component2/FileHandler.java
+++ src/main/java/com/demo/parkingmanagementsystem/component2/FileHandler.java
@@ -4,23 +4,45 @@ import java.io.*;
 import java.util.*;
 
 public class FileHandler {
+
+    //file location where slot data is stored
     private static final String FILE_PATH = "src/main/resources/templates/slots.txt";
 
-    // Create: Save a new slot
+    //save slot data
+
+    //Save a parking slot into the text file 
     public static void saveSlot(ParkingSlot slot) {
+
+        //add data without deleting old data
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+
+            //Convert slot object into text 
             writer.write(slot.toString());
             writer.newLine();
+
+            //print error if something fails
         } catch (IOException e) { e.printStackTrace(); }
     }
 
-    // Read: Get all slots to check availability
+    // Read: Get all slots records from the file
     public static List<String> getAllSlots() {
+
+        //create empty list to store slot data
         List<String> slots = new ArrayList<>();
+
+        //open file for reading
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
-            while ((line = reader.readLine()) != null) { slots.add(line); }
+
+            //read file line by line
+            while ((line = reader.readLine()) != null) { 
+
+                //add each line into the list
+                slots.add(line);
+            }
         } catch (IOException e) { /* File might not exist yet */ }
+
+        //return all slot records
         return slots;
     }
 }

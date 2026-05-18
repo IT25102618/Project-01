92703c71085e34cac0cd21914f8154f5b3b48647
IT25100699 <it25100699@my.sliit.lk>
2026-05-18 13:28:52 +0530

    component 4

---
+++ src/main/java/org/example/parkingmanagementsystem/ParkingManagementsystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.parkingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManagementsystemApplication.class, args);
+    }
+
+}

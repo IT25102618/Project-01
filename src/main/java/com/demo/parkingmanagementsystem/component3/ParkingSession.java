660983da1ca50c603feebb5ede7245b78c7518de
IT25102618 <IT25102618@my.sliit.lk>
2026-05-19 09:25:12 +0530

    Refine comments in ParkingSession class
    Updated comments to improve clarity on encapsulation, abstraction, and entry time.

--- src/main/java/com/demo/parkingmanagementsystem/component3/ParkingSession.java
+++ src/main/java/com/demo/parkingmanagementsystem/component3/ParkingSession.java
@@ -3,9 +3,10 @@ package com.demo.parkingmanagementsystem.component3;
 import java.time.LocalDateTime;
 
 public class ParkingSession {
-    // Encapsulation: private variables
+    // Encapsulation: private
     private String plate;
     private String pin;
+    //automatic entry time
     private LocalDateTime entryTime;
 
     public ParkingSession(String plate, String pin) {
@@ -14,9 +15,11 @@ public class ParkingSession {
         this.entryTime = LocalDateTime.now();
     }
 
-    // Abstraction: This method handles the logic of formatting [cite: 88]
+    // Abstraction,polymorphism
     @Override
+    //complex data to simple format abstract
     public String toString() {
+        
         return "Plate: " + plate + " | PIN: " + pin + " | Entered: " + entryTime;
     }
 }
